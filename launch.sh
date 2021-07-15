b81239177ec04361c94aaf667c496e378355e503
Raphaël Slinckx <r.slinckx@whatever-company.com>
2021-07-15 11:16:28 +0200

    chore(productboard): More removal of productboard features

--- launch.sh
+++ launch.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ -z "$TO_REF" ]; then
-    /app/dev-sync.py from_productboard to_github
-else
-    /app/dev-sync.py from_github to_productboard to_zendesk to_slack to_datadog
-fi
+/app/dev-sync.py from_github to_zendesk to_slack to_datadog
